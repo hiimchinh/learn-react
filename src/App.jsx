cf95143e01b4d374a7f1dd0f3370fbd187ee1306
hiimchinh <chinhhm@stringee.com>
2023-12-03 22:37:35 +0700

    add key to shopping list item

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css'
-import ColorList from './ColorList'
-import DoubleDice from './DoubleDice'
-import ShoppingList from './ShoppingList'
-import Slots from './Slots'
-
+import "./App.css";
+import ColorList from "./ColorList";
+import DoubleDice from "./DoubleDice";
+import ShoppingList from "./ShoppingList";
+import Slots from "./Slots";
 
 function App() {
   const shoppingListData = [
-    {name: 'eggs', quantity: 12, completed: false},
-    {name: 'milk', quantity: 24, completed: true},
-    {name: 'chicken breast', quantity: 4, completed: false},
-    {name: 'carrots', quantity: 30, completed: true},
+    { id: 1, name: "eggs", quantity: 12, completed: false },
+    { id: 2, name: "milk", quantity: 24, completed: true },
+    { id: 3, name: "chicken breast", quantity: 4, completed: false },
+    { id: 4, name: "carrots", quantity: 30, completed: true },
   ];
   return (
     <>
-      <ShoppingList items={shoppingListData}/>
+      <ShoppingList items={shoppingListData} />
       {/* <Slots val1="1" val2="2" val3="3"/>
       <Slots val1="1" val2="1" val3="1"/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -1,9 +1,11 @@
 import ShoppingListItem from "./ShoppingListItem";
 
 export default function ShoppingList({ items }) {
-    return (
-        <ul>
-            {items.map((item) => <ShoppingListItem item={item}/>)}
-        </ul>
-    )
+  return (
+    <ul>
+      {items.map((item) => (
+        <ShoppingListItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
 }
