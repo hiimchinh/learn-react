c35f58268f26e3c2fdfd292984a0a5fee6a7411d
hiimchinh <chinhhm@proton.me>
2023-12-11 09:49:51 +0700

    pass win check function for LuckyN

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import LuckyN from "./LuckyN/LuckyN.jsx";
+import { sumDice } from "./LuckyN/utils.js";
 
 function App() {
+    const lessThan4 = (dice) => {
+        return sumDice(dice) < 4;
+    };
+    const allSameValues = (dice) => {
+        return dice.every((v) => v === dice[0]);
+    };
     return (
         <>
-            <LuckyN />
+            <LuckyN winCheck={lessThan4} title="Less than 4" />
+            <LuckyN
+                numDice={3}
+                winCheck={allSameValues}
+                title="All same value dice"
+            />
         </>
     );
 }
--- src/LuckyN/LuckyN.jsx
+++ src/LuckyN/LuckyN.jsx
@@ -1,18 +1,20 @@
 import Dice from "./Dice.jsx";
 import { useState } from "react";
-import { getRolls, sumDice } from "./utils.js";
+import { getRolls } from "./utils.js";
 import "./LuckyN.css";
 
-function LuckyN({ numDice = 2, goal = 7 }) {
+function LuckyN({ numDice = 2, winCheck, title = "Dice Roll:" }) {
     const [dice, setDice] = useState(getRolls(numDice));
-    const hasWon = sumDice(dice) === goal;
+    const hasWon = winCheck(dice);
 
     const reRoll = () => {
         setDice(getRolls(numDice));
     };
     return (
         <main className="LuckyN">
-            <h1>Roll dice game: {hasWon && "You won!!"}</h1>
+            <h1>
+                {title}: {hasWon && "You won!!"}
+            </h1>
             <Dice dice={dice} />
             <button onClick={reRoll}>Re roll</button>
         </main>
