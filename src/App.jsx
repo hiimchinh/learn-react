5c66b2fefad51db97e0a2ff9340f647704a66f31
hiimchinh <chinhhm@proton.me>
2023-12-21 20:48:23 +0700

    add NavBar using AppBar from mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import FormDemo from "./mui/FormDemo.jsx";
+import Navbar from "./mui/Navbar.jsx";
 
 function App() {
     return (
         <>
-            <FormDemo />
+            <Navbar />
         </>
     );
 }
